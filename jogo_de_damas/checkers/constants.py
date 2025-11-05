49193d4accd370d50d343df6dd710abc6ecf68cb
LucasSantiago257 <lucascarvalhosantiago257@gmail.com>
2025-11-05 09:06:33 -0300

    Initial Commit - Adding many Files...

---
+++ jogo_de_damas/checkers/constants.py
@@ -0,0 +1,18 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+
+#definir cores
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+# Testando cores
+PERU = (205,133,63)
+SADDLE_BROWN = (139,69,19)
+
+# Cores para destaque
+GREEN_HIGHLIGHT = (144, 238, 144)
