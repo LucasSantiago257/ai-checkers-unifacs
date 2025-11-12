08e13906e5e9f5a10e5ac1b7a68a2a9cebac1a52
Lucas Carvalho Santiago <lucascarvalhosantiago257@gmail.com>
2025-11-12 09:30:59 -0300

    fix: ensure valid moves are drawn during game update and clean up select method

--- jogo_de_damas/checkers/game.py
+++ jogo_de_damas/checkers/game.py
@@ -11,6 +11,7 @@ class Game:
         
     def update(self):
         self.board.draw(self.win)
+        self.draw_valid_moves(self.valid_moves)
         pygame.display.update()
     
     def _init(self):
@@ -28,22 +29,30 @@ class Game:
             if not result:
                 self.selected = None
                 self.select(row, col)
-        else:
-            piece = self.board.get_piece(row, col)
-            if piece != 0 and piece.color == self.turn:
-                self.selected = piece
-                self.valid_moves = self.board.get_valid_moves(piece)
-                return True
-            return False
+        
+        piece = self.board.get_piece(row, col)
+        if piece != 0 and piece.color == self.turn:
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
+        return False
 
     def move(self, row, col):
         piece = self.board.get_piece(row, col)
         if self.selected and (row, col) in self.valid_moves:
             self.board.move(self.selected, row, col)
+            skipped = self.valid_moves[(row, col)]
+            if skipped:
+                self.board.remove(skipped)
             self.change_turn()
         else:
             return False
         return True
+    
+    def draw_valid_moves(self, moves):
+        for move in moves:
+            row, col = move
+            pygame.draw.circle(self.win, GREEN_HIGHLIGHT, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 15)
     
     def change_turn(self):
         self.valid_moves = {}
