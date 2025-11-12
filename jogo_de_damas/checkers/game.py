2b6d75e02866f69d7eadd8cfa23eb2b33e864fee
Lucas Carvalho Santiago <lucascarvalhosantiago257@gmail.com>
2025-11-11 23:08:28 -0300

    feat: implement Game class with core functionality for checkers

---
+++ jogo_de_damas/checkers/game.py
@@ -0,0 +1,53 @@
+from .board import Board
+from .constants import *
+from .board import Board
+
+import pygame
+
+class Game:
+    def __init__(self, win):
+        self._init()
+        self.win = win
+        
+    def update(self):
+        self.board.draw(self.win)
+        pygame.display.update()
+    
+    def _init(self):
+        self.selected = None
+        self.board = Board()
+        self.turn = AZUL_MARINHO
+        self.valid_moves = {}
+    
+    def reset(self):
+        self._init()
+    
+    def select(self, row, col):
+        if self.selected:
+            result = self.move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+        else:
+            piece = self.board.get_piece(row, col)
+            if piece != 0 and piece.color == self.turn:
+                self.selected = piece
+                self.valid_moves = self.board.get_valid_moves(piece)
+                return True
+            return False
+
+    def move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        if self.selected and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col)
+            self.change_turn()
+        else:
+            return False
+        return True
+    
+    def change_turn(self):
+        self.valid_moves = {}
+        if self.turn == AZUL_MARINHO:
+            self.turn = WHITE
+        else:
+            self.turn = AZUL_MARINHO
