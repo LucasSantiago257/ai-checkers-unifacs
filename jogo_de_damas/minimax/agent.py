32f3bcba1c7f2cad8f126825df70ceefbfe337da
Lucas Carvalho Santiago <lucascarvalhosantiago257@gmail.com>
2025-11-14 17:34:38 -0300

    feat: add minimax agent for checkers game

--- jogo_de_damas/minimax/agent.py
+++ jogo_de_damas/minimax/agent.py
@@ -1,3 +1,13 @@
-class Agent:
-    def __init__(self):
-        pass
+from copy import deepcopy
+import pygame
+from jogo_de_damas.checkers.constants import *
+from jogo_de_damas.checkers.game import Game
+
+def minimax(position, depth, max_player, game):
+    if depth == 0 or position.winner() != None:
+        return position.evaluate(), position
+    
+    if max_player:
+        max_eval = float('-inf')
+    
+    
